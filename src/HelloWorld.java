fae08acadd549530b68f21d9bac502e3c2d444f0
Ranjit <ranjit_gill@outlook.com>
2019-01-01 12:39:27 +0000

    first Commit-Sample Programs

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
-
+//Program to Print Hello on the Console
 public class HelloWorld {
 	
 	public static void main(String args[]) {
 		
-		System.out.println("Hello..World!!!");
+		System.out.println("Hello..World!!! This is first Java Program");
 	}
 
 }
