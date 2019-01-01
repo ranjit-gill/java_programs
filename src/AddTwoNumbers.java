acd13ff8a08adb8dbd09717b1a6f8e996b256ba7
ranjit-gill <44748186+ranjit-gill@users.noreply.github.com>
2019-01-01 20:03:14 +0530

    Add Two Numbers

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 16;
+		int c = a + b;
+		System.out.println("The Sum of two numbers "+ a + " and " + b + " is "+c);
+	}
+
+}
