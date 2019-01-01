19ce81b5d0d5a8aa05c4f365492ace51fdd6520e
ranjit-gill <44748186+ranjit-gill@users.noreply.github.com>
2019-01-01 18:18:35 +0530

    String Comparison

---
+++ src/StringComparison.java
@@ -0,0 +1,15 @@
+
+/* Program to compares two strings by using str compareTo (string), str compareToIgnoreCase(String) 
+ * and str compareTo(object string) of string class 
+ * and returns the ascii difference of first odd characters of compared strings.*/
+
+public class StringComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string1 = "Hello World";
+		String string2 = "hello world";
+		
+	}
+
+}
