ea0b8cc002c73f6a914d6145d97947b1d70701e0
rgill <rgill@RGILLPF0ZDJJ.opentext.net>
2019-01-05 15:27:24 +0530

    Get IP Adress of Current Host

---
+++ src/GetIPAddress.java
@@ -0,0 +1,11 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class GetIPAddress {
+
+	@SuppressWarnings("unused")
+	private static void main (String[] args) throws UnknownHostException {
+		InetAddress ip = InetAddress.getLocalHost();
+		System.out.println(ip);
+	}
+}
